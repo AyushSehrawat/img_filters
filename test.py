446d6398012efab51509530700344e8486aec4e8
Ayush <scientificnationwords@gmail.com>
2021-01-26 12:45:39 +0530

    Some changes

--- test.py
+++
@@ -1,8 +0,0 @@
-from img_filters.color import Color
-from img_filters.filter import Filter
-from img_filters.tools import Tools
-a = Color('daniel.webp')
-b = Filter('daniel.webp')
-c = Tools('daniel.webp')
-
-# A test file you can try 
